002a27606172a9269b8f17f6b442a41e55386fc1
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-20 15:36:45 -0700

    adding NSData and NSString categories.

---
+++ Mine/Categories/NSString+Helpers.h
@@ -0,0 +1,43 @@
+//
+//  NSString+Helpers.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <CommonCrypto/CommonCryptor.h>
+#import <CommonCrypto/CommonDigest.h>
+
+#import "NSData+Crypto.h"
+
+/**
+ * Extension of the NSString class to add custom helpers
+ */
+@interface NSString (Helpers) 
+
+/**
+ * URI encode HTML characters in the given parameters
+ */
+- (NSString*)stringByEncodingHTMLCharacters;
+
+/**
+ * Encrytion with a pre-decided key and AES128
+ */
+- (NSString*)encrypt;
+
+/**
+ * Computes sha256 
+ */
+- (NSData*)sha256;
+
+/**
+ * Generates a random alphanumeric string of variable length
+ */
++ (NSString*)randomString;
+
+/**
+ * Generate random alphanumeric string of the given length
+ */
++ (NSString*)stringWithRandomnessOfLength:(NSInteger)length;
+
+@end
