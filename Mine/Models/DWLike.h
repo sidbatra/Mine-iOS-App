6e02d411baea6610d441a7770db0f58f456ef8c3
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-28 14:51:31 -0700

    adding like model.

---
+++ Mine/Models/DWLike.h
@@ -0,0 +1,22 @@
+//
+//  DWLike.h
+//  Mine
+//
+//  Created by Siddharth Batra on 7/28/12.
+//  Copyright (c) 2012 Denwen, Inc. All rights reserved.
+//
+
+#import "DWPoolObject.h"
+
+@class DWUser;
+
+@interface DWLike : DWPoolObject {
+    DWUser      *_user;
+}
+
+/**
+ * The user who created the like.
+ */
+@property (nonatomic,strong) DWUser *user;
+
+@end
---
+++ Mine/Models/DWLike.m
@@ -0,0 +1,67 @@
+//
+//  DWLike.m
+//  Mine
+//
+//  Created by Siddharth Batra on 7/28/12.
+//  Copyright (c) 2012 Denwen, Inc. All rights reserved.
+//
+
+#import "DWLike.h"
+
+#import "DWUser.h"
+#import "DWConstants.h"
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+@implementation DWLike
+
+@synthesize user = _user;
+
+
+//----------------------------------------------------------------------------------------------------
+- (id)init {
+	self = [super init];
+	
+	if(self) {  
+    }
+	
+	return self;  
+}
+
+//----------------------------------------------------------------------------------------------------
+-(void)dealloc{
+	[self freeMemory];
+    
+    [self.user destroy];
+    
+	NSLog(@"Like released %d",self.databaseID);
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)freeMemory {
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)update:(NSDictionary*)like {
+    [super update:like];
+    
+    NSDictionary *user = [like objectForKey:kKeyUser];
+    
+    if(user) {
+        if(self.user)
+            [self.user update:user];
+        else
+            self.user = [DWUser create:user];
+    }
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)debug {
+    NSLog(@"%d",self.databaseID);
+    [self.user debug];
+}
+
+@end
