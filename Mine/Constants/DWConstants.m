24368a542e79b632f93deef7bcf35e28be83f6b1
Deepak Rao <raodpk@gmail.com>
2012-08-16 18:06:04 -0700

    year change reverted

--- Mine/Constants/DWConstants.m
+++ Mine/Constants/DWConstants.m
@@ -2,7 +2,7 @@
 //  DWConstants.m
 //  Mine
 //
-//  Created by Siddharth Batra on 7/20/15.
+//  Created by Siddharth Batra on 7/20/12.
 //  Copyright (c) 2012 Denwen, Inc. All rights reserved.
 //
 
