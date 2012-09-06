0efe26e101134fd70e111fdaefdcb59dfab742ee
Siddharth Batra <siddharthabatra@gmail.com>
2012-09-06 16:20:55 +0530

    nav title view on profile tab.

--- Mine/Containers/DWProfileNavigationViewController.m
+++ Mine/Containers/DWProfileNavigationViewController.m
@@ -9,7 +9,10 @@
 #import "DWProfileNavigationViewController.h"
 
 #import "DWProfileViewController.h"
+#import "DWNavigationBarTitleView.h"
 #import "DWSession.h"
+#import "DWConstants.h"
+
 
 
 //----------------------------------------------------------------------------------------------------
@@ -17,12 +20,19 @@
 //----------------------------------------------------------------------------------------------------
 @interface DWProfileNavigationViewController() {
     DWProfileViewController *_profileViewController;
+    
+    DWNavigationBarTitleView *_navTitleView;
 }
 
 /**
  * Table view controller for displaying the profile.
  */
 @property (nonatomic,strong) DWProfileViewController *profileViewController;
+
+/**
+ * Tile view inserted onto the navigation bar.
+ */
+@property (nonatomic,strong) DWNavigationBarTitleView *navTitleView;
 
 @end
 
@@ -33,7 +43,8 @@
 //----------------------------------------------------------------------------------------------------
 @implementation DWProfileNavigationViewController
 
-@synthesize profileViewController = _profileViewController;
+@synthesize profileViewController   = _profileViewController;
+@synthesize navTitleView            = _navTitleView;
 
 //----------------------------------------------------------------------------------------------------
 - (void)awakeFromNib {
@@ -50,13 +61,19 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
     
-    self.navigationItem.title = @"Profile";
+    self.navigationItem.title = @"";
     
     
     if(!self.profileViewController) {
         self.profileViewController = [[DWProfileViewController alloc] initWithUser:[DWSession sharedDWSession].currentUser];
         self.profileViewController.delegate = self;
     }
+    
+    if(!self.navTitleView) {
+        self.navTitleView =  [[DWNavigationBarTitleView alloc] initWithFrame:CGRectMake(121,0,76,44)
+                                                                andImageName:kNavBarMineLogo];
+    }
+    
     
     [self.view addSubview:self.profileViewController.view];
 }
@@ -65,4 +82,18 @@
 - (void)viewDidUnload {
     [super viewDidUnload];
 }
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+#pragma mark -
+#pragma mark Nav Stack Selectors
+
+//----------------------------------------------------------------------------------------------------
+- (void)willShowOnNav {
+    [self.navigationController.navigationBar addSubview:self.navTitleView];
+}
+
 @end
