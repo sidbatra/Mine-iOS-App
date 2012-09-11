80e821e430629e3aaa4d78b76ff75ccd86086f49
Deepak Rao <raodpk@gmail.com>
2012-09-11 12:20:42 -0700

    positioning tumblr & twitter connect

--- Mine/Managers/DWGUIManager.m
+++ Mine/Managers/DWGUIManager.m
@@ -18,13 +18,15 @@
 //----------------------------------------------------------------------------------------------------
 + (UILabel*)navBarTitleViewWithText:(NSString*)text {
     
-    UILabel *titleLabel            = [[UILabel alloc] initWithFrame:CGRectMake(10,4,180,18)];
-    titleLabel.textColor           = [UIColor whiteColor];
-    titleLabel.textAlignment       = UITextAlignmentCenter;
-    titleLabel.backgroundColor     = [UIColor clearColor];
-    titleLabel.font                = [UIFont fontWithName:@"HelveticaNeue-Bold" 
-                                                     size:17];
-    titleLabel.text                = text;
+    UILabel *titleLabel             = [[UILabel alloc] initWithFrame:CGRectMake(10,3,180,22)];
+    titleLabel.textColor            = [UIColor whiteColor];
+    titleLabel.textAlignment        = UITextAlignmentCenter;
+    titleLabel.backgroundColor      = [UIColor clearColor];
+    titleLabel.shadowColor          = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.48];
+    titleLabel.shadowOffset         = CGSizeMake(0, 1);
+    titleLabel.font                 = [UIFont fontWithName:@"HelveticaNeue-Bold" 
+                                                     size:20];
+    titleLabel.text                 = text;
     
     return titleLabel;
 }
