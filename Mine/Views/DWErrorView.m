fa3f0c151bb0b6c3dd0b58ce48b39c1f7a2ea2b7
Siddharth Batra <siddharthabatra@gmail.com>
2012-09-27 18:33:28 -0700

    error view styled.

--- Mine/Views/DWErrorView.m
+++ Mine/Views/DWErrorView.m
@@ -4,10 +4,11 @@
 //
 
 #import "DWErrorView.h"
+#import "DWDevice.h"
 
 
 static NSInteger const kSpinnerSize     = 20;
-static NSString* const kImgRefresh      = @"post_retry.png";
+static NSString* const kImgRefresh      = @"refresh-gray.png";
 static NSString* const kMsgRefreshText  = @"Try again";
 
 
@@ -16,30 +17,10 @@ static NSString* const kMsgRefreshText  = @"Try again";
  */
 @interface DWErrorView()
 
-/**
- * Create and add the text label to the view
- */
+
 - (void)createText;
-
-/**
- * Create a textured background view using an image
- */
-- (void)createBackground;
-
-/**
- * Refresh icon
- */
 - (void)createRefreshImage;
-
-/**
- * Label to display refresh text
- */
 - (void)createRefreshText;
-
-/**
- * Create an invisible button to handle clicks anywhere
- * on the view
- */
 - (void)createViewButton;
 
 @end
@@ -58,9 +39,8 @@ static NSString* const kMsgRefreshText  = @"Try again";
     self = [super initWithFrame:frame];
     
     if (self) {
-        self.backgroundColor =  [UIColor blackColor];
+        self.backgroundColor =  [UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1.0];
         
-        //[self createBackground];
         [self createText];
         [self createRefreshImage];
         [self createRefreshText];
@@ -76,28 +56,23 @@ static NSString* const kMsgRefreshText  = @"Try again";
 - (void)createText {
     
     messageLabel                   = [[UILabel alloc] initWithFrame:CGRectMake(0,
-                                                                                 self.frame.size.height / 2 - 35 - 49,
-                                                                                 self.frame.size.width,
-                                                                                 20)];	
+                                                                               [DWDevice sharedDWDevice].screenHeightMinusStatusBar / 2 - [DWDevice sharedDWDevice].navBarHeight -10 - 14 - 11,
+                                                                               self.frame.size.width,
+                                                                               20)];	
 	messageLabel.backgroundColor	= [UIColor clearColor];
 	messageLabel.font				= [UIFont fontWithName:@"HelveticaNeue" size:17];	
-	messageLabel.textColor			= [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
+	messageLabel.textColor			= [UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:1.0];
 	messageLabel.textAlignment		= UITextAlignmentCenter;
     
 	[self addSubview:messageLabel];
 }
 
 //----------------------------------------------------------------------------------------------------
-- (void)createBackground {
-	//[self addSubview:[DWGUIManager backgroundImageViewWithFrame:self.frame]];
-}
-
-//----------------------------------------------------------------------------------------------------
 - (void)createRefreshImage {
-    refreshImageView                    = [[UIImageView alloc] initWithFrame:CGRectMake(112,
-                                                                                         self.frame.size.height / 2 - 9 - 49,
-                                                                                         13,
-                                                                                         15)];	
+    refreshImageView                    = [[UIImageView alloc] initWithFrame:CGRectMake(116.5,
+                                                                                        [DWDevice sharedDWDevice].screenHeightMinusStatusBar / 2 - [DWDevice sharedDWDevice].navBarHeight - 14,
+                                                                                        13,
+                                                                                        15)];
 	refreshImageView.image              = [UIImage imageNamed:kImgRefresh];
 	
 	[self addSubview:refreshImageView];
@@ -105,14 +80,14 @@ static NSString* const kMsgRefreshText  = @"Try again";
 
 //----------------------------------------------------------------------------------------------------
 - (void)createRefreshText {
-    refreshLabel                    = [[UILabel alloc] initWithFrame:CGRectMake(8,
-                                                                                 self.frame.size.height / 2 - 10 - 49,
-                                                                                 self.frame.size.width,
+    refreshLabel                    = [[UILabel alloc] initWithFrame:CGRectMake(138.5,
+                                                                                 [DWDevice sharedDWDevice].screenHeightMinusStatusBar / 2 - [DWDevice sharedDWDevice].navBarHeight - 14 - 2,
+                                                                                 90,
                                                                                  20)];	
 	refreshLabel.backgroundColor	= [UIColor clearColor];
 	refreshLabel.font				= [UIFont fontWithName:@"HelveticaNeue-Bold" size:17];	
-	refreshLabel.textColor			= [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
-	refreshLabel.textAlignment		= UITextAlignmentCenter;
+	refreshLabel.textColor			= [UIColor colorWithRed:0.454 green:0.454 blue:0.454 alpha:1.0];
+	refreshLabel.textAlignment		= UITextAlignmentLeft;
     refreshLabel.text               = kMsgRefreshText;
     
 	[self addSubview:refreshLabel];
