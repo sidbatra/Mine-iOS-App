922e434bf39bb1f90d62ed22ef29979b6dfb5644
Siddharth Batra <siddharthabatra@gmail.com>
2012-09-29 20:13:03 +0000

    script to auto generate 1x assets.

---
+++ script/resize.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'mini_magick'
+
+BASE_FOLDER = "Mine/Assets/"
+TWO_X = "@2x"
+SIZED = "-sized"
+
+Dir.glob("#{BASE_FOLDER}*").each do |file|
+
+next unless file.match(TWO_X) || file.match(SIZED)
+
+puts file
+
+image = MiniMagick::Image.open(file)
+image.resize "50%"
+image.write file.gsub(TWO_X,"")
+
+end
