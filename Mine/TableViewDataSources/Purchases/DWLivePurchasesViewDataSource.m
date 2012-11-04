e6c3a63962a2027f924c4e2dd6a1ae781a142e4e
Siddharth Batra <siddharthabatra@gmail.com>
2012-11-03 18:42:54 -0700

    live purchases data source logic.

--- Mine/TableViewDataSources/Purchases/DWLivePurchasesViewDataSource.m
+++ Mine/TableViewDataSources/Purchases/DWLivePurchasesViewDataSource.m
@@ -8,12 +8,24 @@
 
 #import "DWLivePurchasesViewDataSource.h"
 
+static NSInteger const kInitialRetryInterval = 10;
+static NSInteger const kDefaultRetryInterval = 3;
+static CGFloat const kRetryDelta = 1.5;
+static NSInteger const kMaxTries = 3;
+
 
 @interface DWLivePurchasesViewDataSource() {
+    BOOL _isInitialTry;
+    
     NSInteger _offset;
+    CGFloat   _retryInterval;
+    NSInteger _tries;
 }
 
+@property (nonatomic,assign) BOOL isInitialTry;
 @property (nonatomic,assign) NSInteger offset;
+@property (nonatomic,assign) CGFloat retryInterval;
+@property (nonatomic,assign) NSInteger tries;
 
 @end
 
@@ -31,20 +43,35 @@
     self = [super init];
     
     if(self) {
+        self.isInitialTry = YES;
+        self.retryInterval = kDefaultRetryInterval;
     }
     
     return self;
 }
 
 //----------------------------------------------------------------------------------------------------
-- (void)loadPurchases {
+- (void)loadDelayedPurchases {
     [self.purchasesController getUnapprovedLivePurchasesAtOffset:self.offset
                                                          perPage:10];
 }
 
 //----------------------------------------------------------------------------------------------------
+- (void)loadPurchases {
+        
+    [self performSelector:@selector(loadDelayedPurchases)
+               withObject:nil
+               afterDelay:self.isInitialTry ? kInitialRetryInterval : self.retryInterval];
+    
+    self.isInitialTry = NO;
+}
+
+//----------------------------------------------------------------------------------------------------
 - (void)refreshInitiated {
+    self.isInitialTry = YES;
     self.offset = 0;
+    self.tries = 0;
+    self.retryInterval = kInitialRetryInterval;
     
     [super refreshInitiated];
 }
@@ -61,11 +88,23 @@
 
 //----------------------------------------------------------------------------------------------------
 - (void)unapprovedPurchasesLoaded:(NSMutableArray *)purchases {
-    [super unapprovedPurchasesLoaded:purchases];
     
     if([purchases count]) {
+        [super unapprovedPurchasesLoaded:purchases];
+        
+        self.tries = 0;
         self.offset += [purchases count];
+        self.retryInterval = kDefaultRetryInterval;
+        
         [self loadPurchases];
+    }
+    else if(self.tries++ < kMaxTries ) {
+        self.retryInterval *= kRetryDelta;
+        
+        [self loadPurchases];
+    }
+    else {
+        [super unapprovedPurchasesLoaded:purchases];
     }
 }
 
